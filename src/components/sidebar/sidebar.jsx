33867f65926c0c9d86de03c96e0330f6e69d2431
mayank <jimayank2105@gmail.com>
2026-08-03 19:01:15 +0530

    add navbar section

--- src/components/sidebar/sidebar.jsx
+++ src/components/sidebar/sidebar.jsx
@@ -1,11 +1,19 @@
 import React from 'react'
 import styles from './sidebar.module.css'
+import DocumentScannerIcon from '@mui/icons-material/DocumentScanner';
 const sidebar = () => {
   return (
     <div className={styles.sidebar}>
       <div className={styles.sidebarIcon}>
-        <div>Resume Analyzer</div>
+        <DocumentScannerIcon sx={{ fontSize: 54, marginBottom: 2 }} />
+        <div className={styles.sidebarTopContent}>Resume Analyzer</div>
       </div>
+
+      <div className={styles.sidebarOptionblock}>
+        <div className={styles.sidebarOption}>
+            DashBoard
+        </div>
+        </div>
     </div>
   )
 }
