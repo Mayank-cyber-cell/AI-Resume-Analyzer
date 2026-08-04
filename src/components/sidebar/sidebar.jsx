382f4690f6f447ac756b32ebdc56f9eab12bbadd
mayank <jimayank2105@gmail.com>
2026-08-04 17:39:40 +0530

    adding sections in navbar

--- src/components/sidebar/sidebar.jsx
+++ src/components/sidebar/sidebar.jsx
@@ -1,6 +1,10 @@
 import React from 'react'
 import styles from './sidebar.module.css'
 import DocumentScannerIcon from '@mui/icons-material/DocumentScanner';
+import DashboardCustomizeIcon from '@mui/icons-material/DashboardCustomize';
+import ManageHistoryIcon from '@mui/icons-material/ManageHistory';
+import AdminPanelSettingsIcon from '@mui/icons-material/AdminPanelSettings';
+import LogoutIcon from '@mui/icons-material/Logout';
 const sidebar = () => {
   return (
     <div className={styles.sidebar}>
@@ -10,10 +14,28 @@ const sidebar = () => {
       </div>
 
       <div className={styles.sidebarOptionblock}>
+
         <div className={styles.sidebarOption}>
-            DashBoard
+          <DashboardCustomizeIcon sx={{ fontSize: 28 }} />
+          <div>Dashboard</div>
         </div>
+
+        <div className={styles.sidebarOption}>
+          <ManageHistoryIcon sx={{ fontSize: 28 }} />
+          <div>History</div>
         </div>
+
+        <div className={styles.sidebarOption}>
+          <AdminPanelSettingsIcon sx={{ fontSize: 28 }} />
+          <div>Admin</div>
+        </div>
+
+        <div className={styles.sidebarOption}>
+          <LogoutIcon sx={{ fontSize: 28 }} />
+          <div>LogOut</div>
+        </div>
+
+      </div>
     </div>
   )
 }
