68dc8f0f98031dbb02112ee322b741a1ac6eb27d
mayank <jimayank2105@gmail.com>
2026-08-05 18:51:08 +0530

    Update

--- src/components/DashBoard/DashBoard.jsx
+++ src/components/DashBoard/DashBoard.jsx
@@ -8,6 +8,23 @@ const DashBoard = () => {
           <div className={styles.DashboardHeaderTitle}>A.I Resume Analyzer</div>
           <div className={styles.DashboardHeaderLargeTitle}>Analyze your Resume with AI</div>
         </div>
+        <div className={styles.alertInfo}>
+          <div>🔔 Important Instructions:</div>
+          <div className={styles.dashboardInstruction}>
+            <div>📝Please paste the complete Job Description in 'Job Description' field Before analyzing your resume.</div>
+            <div>📄 Only PDF files are accepted for analysis.</div>
+          </div>
+        </div>
+
+        <div className={styles.DashboardUploadResume}>
+          <div className={styles.DashboardResumeBlock}> Upload Your Resume</div>
+          <div className={styles.DashboardInputField}>
+            <label htmlFor="inputField" className={styles.analyzeAIBtn}>
+              Upload Resume
+            </label>
+            <input type="file" id="inputField" accept=".pdf" />
+          </div>
+        </div>
       </div>
     </div>
   )
