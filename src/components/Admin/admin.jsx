29b1d227c9389aa14dabbf0771b220fab0b7abaa
mayank <jimayank2105@gmail.com>
2026-08-08 17:23:05 +0530

    Update in The History And Admin Section

--- src/components/Admin/admin.jsx
+++ src/components/Admin/admin.jsx
@@ -1,9 +1,66 @@
 import React from 'react'
 import styles from './admin.module.css'
+import { Skeleton } from '@mui/material'
 const admin = () => {
   return (
-    <div>
-      
+    <div className={styles.Admin}>
+      <div className={styles.AdminBlock}>
+        <Skeleton
+          variant="rectangular"
+          width={210}
+          height={450}
+          sx={{ borderRadius: '20px' }} />
+        <div className={styles.AdminCard}>
+          <h2>Mayank shah</h2>
+          <p style={{ color: "blue" }}>Jimayank2105@gmail.com</p>
+          <h3>Score: 77%</h3>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos enim non explicabo tempora assumenda vitae odit recusandae. Aliquid fuga libero ducimus atque eum assumenda ea sapiente consequatur numquam sint magni dolor consequuntur, facere inventore debitis temporibus magnam accusantium repellat veniam, rerum incidunt fugiat tempore. Odio vitae provident delectus atque dolore, dolor a magni cumque ipsam sint obcaecati, esse accusantium!</p>
+        </div>
+
+        <div className={styles.AdminCard}>
+          <h2>Mayank shah</h2>
+          <p style={{ color: "blue" }}>Jimayank2105@gmail.com</p>
+          <h3>Score: 77%</h3>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos enim non explicabo tempora assumenda vitae odit recusandae. Aliquid fuga libero ducimus atque eum assumenda ea sapiente consequatur numquam sint magni dolor consequuntur, facere inventore debitis temporibus magnam accusantium repellat veniam, rerum incidunt fugiat tempore. Odio vitae provident delectus atque dolore, dolor a magni cumque ipsam sint obcaecati, esse accusantium!</p>
+        </div>
+
+        <div className={styles.AdminCard}>
+          <h2>Mayank shah</h2>
+          <p style={{ color: "blue" }}>Jimayank2105@gmail.com</p>
+          <h3>Score: 77%</h3>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos enim non explicabo tempora assumenda vitae odit recusandae. Aliquid fuga libero ducimus atque eum assumenda ea sapiente consequatur numquam sint magni dolor consequuntur, facere inventore debitis temporibus magnam accusantium repellat veniam, rerum incidunt fugiat tempore. Odio vitae provident delectus atque dolore, dolor a magni cumque ipsam sint obcaecati, esse accusantium!</p>
+        </div>
+
+        <div className={styles.AdminCard}>
+          <h2>Mayank shah</h2>
+          <p style={{ color: "blue" }}>Jimayank2105@gmail.com</p>
+          <h3>Score: 77%</h3>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos enim non explicabo tempora assumenda vitae odit recusandae. Aliquid fuga libero ducimus atque eum assumenda ea sapiente consequatur numquam sint magni dolor consequuntur, facere inventore debitis temporibus magnam accusantium repellat veniam, rerum incidunt fugiat tempore. Odio vitae provident delectus atque dolore, dolor a magni cumque ipsam sint obcaecati, esse accusantium!</p>
+        </div>
+
+        <div className={styles.AdminCard}>
+          <h2>Mayank shah</h2>
+          <p style={{ color: "blue" }}>Jimayank2105@gmail.com</p>
+          <h3>Score: 77%</h3>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos enim non explicabo tempora assumenda vitae odit recusandae. Aliquid fuga libero ducimus atque eum assumenda ea sapiente consequatur numquam sint magni dolor consequuntur, facere inventore debitis temporibus magnam accusantium repellat veniam, rerum incidunt fugiat tempore. Odio vitae provident delectus atque dolore, dolor a magni cumque ipsam sint obcaecati, esse accusantium!</p>
+        </div>
+
+        <div className={styles.AdminCard}>
+          <h2>Mayank shah</h2>
+          <p style={{ color: "blue" }}>Jimayank2105@gmail.com</p>
+          <h3>Score: 77%</h3>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos enim non explicabo tempora assumenda vitae odit recusandae. Aliquid fuga libero ducimus atque eum assumenda ea sapiente consequatur numquam sint magni dolor consequuntur, facere inventore debitis temporibus magnam accusantium repellat veniam, rerum incidunt fugiat tempore. Odio vitae provident delectus atque dolore, dolor a magni cumque ipsam sint obcaecati, esse accusantium!</p>
+        </div>
+
+        <div className={styles.AdminCard}>
+          <h2>Mayank shah</h2>
+          <p style={{ color: "blue" }}>Jimayank2105@gmail.com</p>
+          <h3>Score: 77%</h3>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos enim non explicabo tempora assumenda vitae odit recusandae. Aliquid fuga libero ducimus atque eum assumenda ea sapiente consequatur numquam sint magni dolor consequuntur, facere inventore debitis temporibus magnam accusantium repellat veniam, rerum incidunt fugiat tempore. Odio vitae provident delectus atque dolore, dolor a magni cumque ipsam sint obcaecati, esse accusantium!</p>
+        </div>
+
+
+      </div>
     </div>
   )
 }
--- src/components/History/history.jsx
+++ src/components/History/history.jsx
@@ -1,9 +1,88 @@
 import React from 'react'
 import styles from './history.module.css'
-const History = () => {
+import { Skeleton } from '@mui/material'
+function History() {
   return (
-    <div>
-      History
+    <div className={styles.history}>
+      <div className={styles.HistoryCardBlock}>
+
+        <Skeleton
+          variant="rectangular"
+          width={210}
+          height={444}
+          sx={{ borderRadius: '20px' }} />
+        <div className={styles.HistoryCard}>
+          <div className={styles.CardPercentage}>
+            88%
+          </div>
+          <h2>Frontend Development</h2>
+          <p>Resume Name : Resume.pdf</p>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut ad maiores consequatur vero dicta! Aspernatur, ducimus culpa? Odit, amet ullam. Consequatur vitae dolor obcaecati dolores doloremque soluta? Autem quasi nostrum aliquam fuga debitis incidunt et quidem harum? Ipsam at, illum fugiat autem fuga repudiandae, perferendis aperiam illo doloremque nemo nulla eum rem, doloribus assumenda dolorum eos voluptatum ipsum odit?</p>
+          <p>Dated :2069-01-01</p>
+        </div>
+
+        <div className={styles.HistoryCard}>
+          <div className={styles.CardPercentage}>
+            88%
+          </div>
+          <h2>Frontend Development</h2>
+          <p>Resume Name : Resume.pdf</p>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut ad maiores consequatur vero dicta! Aspernatur, ducimus culpa? Odit, amet ullam. Consequatur vitae dolor obcaecati dolores doloremque soluta? Autem quasi nostrum aliquam fuga debitis incidunt et quidem harum? Ipsam at, illum fugiat autem fuga repudiandae, perferendis aperiam illo doloremque nemo nulla eum rem, doloribus assumenda dolorum eos voluptatum ipsum odit?</p>
+          <p>Dated :2069-01-01</p>
+        </div>
+
+        <div className={styles.HistoryCard}>
+          <div className={styles.CardPercentage}>
+            88%
+          </div>
+          <h2>Frontend Development</h2>
+          <p>Resume Name : Resume.pdf</p>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut ad maiores consequatur vero dicta! Aspernatur, ducimus culpa? Odit, amet ullam. Consequatur vitae dolor obcaecati dolores doloremque soluta? Autem quasi nostrum aliquam fuga debitis incidunt et quidem harum? Ipsam at, illum fugiat autem fuga repudiandae, perferendis aperiam illo doloremque nemo nulla eum rem, doloribus assumenda dolorum eos voluptatum ipsum odit?</p>
+          <p>Dated :2069-01-01</p>
+        </div>
+
+        <div className={styles.HistoryCard}>
+          <div className={styles.CardPercentage}>
+            88%
+          </div>
+          <h2>Frontend Development</h2>
+          <p>Resume Name : Resume.pdf</p>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut ad maiores consequatur vero dicta! Aspernatur, ducimus culpa? Odit, amet ullam. Consequatur vitae dolor obcaecati dolores doloremque soluta? Autem quasi nostrum aliquam fuga debitis incidunt et quidem harum? Ipsam at, illum fugiat autem fuga repudiandae, perferendis aperiam illo doloremque nemo nulla eum rem, doloribus assumenda dolorum eos voluptatum ipsum odit?</p>
+          <p>Dated :2069-01-01</p>
+        </div>
+
+        <div className={styles.HistoryCard}>
+          <div className={styles.CardPercentage}>
+            88%
+          </div>
+          <h2>Frontend Development</h2>
+          <p>Resume Name : Resume.pdf</p>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut ad maiores consequatur vero dicta! Aspernatur, ducimus culpa? Odit, amet ullam. Consequatur vitae dolor obcaecati dolores doloremque soluta? Autem quasi nostrum aliquam fuga debitis incidunt et quidem harum? Ipsam at, illum fugiat autem fuga repudiandae, perferendis aperiam illo doloremque nemo nulla eum rem, doloribus assumenda dolorum eos voluptatum ipsum odit?</p>
+          <p>Dated :2069-01-01</p>
+        </div>
+
+        <div className={styles.HistoryCard}>
+          <div className={styles.CardPercentage}>
+            88%
+          </div>
+          <h2>Frontend Development</h2>
+          <p>Resume Name : Resume.pdf</p>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut ad maiores consequatur vero dicta! Aspernatur, ducimus culpa? Odit, amet ullam. Consequatur vitae dolor obcaecati dolores doloremque soluta? Autem quasi nostrum aliquam fuga debitis incidunt et quidem harum? Ipsam at, illum fugiat autem fuga repudiandae, perferendis aperiam illo doloremque nemo nulla eum rem, doloribus assumenda dolorum eos voluptatum ipsum odit?</p>
+          <p>Dated :2069-01-01</p>
+        </div>
+
+        <div className={styles.HistoryCard}>
+          <div className={styles.CardPercentage}>
+            88%
+          </div>
+          <h2>Frontend Development</h2>
+          <p>Resume Name : Resume.pdf</p>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut ad maiores consequatur vero dicta! Aspernatur, ducimus culpa? Odit, amet ullam. Consequatur vitae dolor obcaecati dolores doloremque soluta? Autem quasi nostrum aliquam fuga debitis incidunt et quidem harum? Ipsam at, illum fugiat autem fuga repudiandae, perferendis aperiam illo doloremque nemo nulla eum rem, doloribus assumenda dolorum eos voluptatum ipsum odit?</p>
+          <p>Dated :2069-01-01</p>
+        </div>
+
+      </div>
+
     </div>
   )
 }
