0da2d2ade211b8135b7e8e75c098c3a3e83d5be7
mayank <jimayank2105@gmail.com>
2026-08-15 08:23:03 +0530

    Update connection.js

--- backend/connection.js
+++ backend/connection.js
@@ -1,6 +1,5 @@
 const mongoose = require("mongoose");
 
-
 mongoose.connect("mongodb+srv://jimayank2105_db_user:PFgNQGEFy1HWD4wW@cluster0.gzvidcv.mongodb.net/?appName=Cluster0").then((res) => {
     console.log("Database connected successfully");
 }).catch((err) => {
